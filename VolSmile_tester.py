5674f16322f1d21795fda97f523c49e23d1683a2
rrajpal293 <67572919+rrajpal293@users.noreply.github.com>
2020-08-09 14:00:25 -0400

    Update VolSmile_tester.py

--- VolSmile_tester.py
+++ VolSmile_tester.py
@@ -4,15 +4,14 @@ import math
 import matplotlib.pyplot as plt
 from numpy import log, exp, sqrt
 from scipy.stats import norm
-# import Option_Vals
 
-rf = 0.01
-rd = 0.01
-t = 1/12
-s0 = 100
-sigma25 = 0.15
-atm = 0.10
-sigma75 = 0.15
+rf = 0.00073
+rd = -0.00357
+t = 3/12
+s0 = 105.92
+sigma25 = 0.0665
+atm = 0.0639
+sigma75 = 0.0739
 
 vs = VolSmile(atm, sigma25, sigma75, t, s0, rd, rf)
 vs.plot()
