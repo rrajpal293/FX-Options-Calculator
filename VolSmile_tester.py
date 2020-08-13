501e9261e319d117b588daf4e59baf17eab26193
rrajpal293 <67572919+rrajpal293@users.noreply.github.com>
2020-08-13 11:14:42 -0400

    Update VolSmile_tester.py

--- VolSmile_tester.py
+++ VolSmile_tester.py
@@ -7,7 +7,7 @@ from scipy.stats import norm
 
 rf = 0.00073
 rd = -0.00357
-t = 3/12
+t = 31/365
 s0 = 105.92
 sigma25 = 0.0665
 atm = 0.0639
@@ -16,4 +16,4 @@ sigma75 = 0.0739
 vs = VolSmile(atm, sigma25, sigma75, t, s0, rd, rf)
 vs.plot()
 call_prices, strikes = vs.calc_option_prices()
-print('The probability of spot being higher than 105 in 3 months is ' + str(vs.calc_prob(105)))
+print('The probability of spot being higher than 105 in 1 month is ' + str(vs.calc_prob(105)))
